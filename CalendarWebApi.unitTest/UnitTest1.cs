dbb168ccce7a26e2f515a8db4414cf8ae86472f9
BayuAnggoroSakti <bayu.anggoro.s@mail.ugm.ac.id>
2023-01-09 10:43:23 +0700

    Prepare Unit Test

---
+++ CalendarWebApi.unitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CalendarWebApi.unitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
